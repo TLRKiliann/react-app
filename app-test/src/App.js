789dfd8c243dba4c3e9630a2dec7ec02af6337d1
TLRKiliann <philogenie@protonmail.com>
2022-05-23 19:19:25 +0200

    some test

--- app-test/src/App.js
+++ app-test/src/App.js
@@ -1,16 +1,68 @@
-
+import React from 'react';
 import './App.css';
 
-function App() {
-  const myList = [1,2,3,4,5];
-  const declaVal = myList.map(a => a)
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    }
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+  }
+
+   increment = () => {
+    const { count } = this.state;
+    this.setState({count: count + 1});
+    }
+
+  decrement() {
+    const { count } = this.state;
+    this.setState({count: count - 1});
+  }
+
+
+  render() {
+    const {
+      count
+    } = this.state;
+
+    return (
+
+      <div className="App">
+        
+        <div id="first--div">
+
+          <button type="button" onClick={(e) => {this.increment(e)}}>
+            +
+          </button>
+
+          <div id="second--div">
+            {count}
+          </div>
+
+          <button onClick={(e) => {this.decrement(e)}}>
+            -
+          </button>
+
+        </div>
+
+        <CallFunction id="third--div" />
+
+      </div>
+    );
+  }
+}
+
+const CallFunction = (props) => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        {declaVal}
-        <p>numbers</p>
-      </header>
+
+    <div id="func--div">
+  
     </div>
+  
   );
 }
 
